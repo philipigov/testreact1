f8cb5f0f05fb8b7db6bb47d3960332f6c28812c5
Philip Carina <philip@carinasciences.com>
2020-03-11 20:39:19 +0100

    Updates

---
+++ precache-manifest.3f33b9527fe580dc377171f3f76e93bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5a7956e8783d091520b38e80e853fc",
+    "url": "/pcla-app/index.html"
+  },
+  {
+    "revision": "00dba263a845d79c9ba4",
+    "url": "/pcla-app/static/css/main.80c9cdca.chunk.css"
+  },
+  {
+    "revision": "3c112bb00208b5683aaf",
+    "url": "/pcla-app/static/js/2.42a46577.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pcla-app/static/js/2.42a46577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00dba263a845d79c9ba4",
+    "url": "/pcla-app/static/js/main.a40dcfc0.chunk.js"
+  },
+  {
+    "revision": "f76921a23d1c3bd49f63",
+    "url": "/pcla-app/static/js/runtime-main.7ad3d26d.js"
+  }
+]);
